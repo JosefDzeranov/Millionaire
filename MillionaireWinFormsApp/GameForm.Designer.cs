09b25f2e601feaa6df6a0d7a8cb407724c02d401
Josef Dzeranov <iodzeranov@mail.ru>
2024-08-22 13:06:19 +0300

    +

--- MillionaireWinFormsApp/GameForm.Designer.cs
+++ MillionaireWinFormsApp/GameForm.Designer.cs
@@ -35,20 +35,27 @@
             answerTextButton4 = new Button();
             label1 = new Label();
             currentWinningLabel = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // questionTextLabel
             // 
-            questionTextLabel.AutoSize = true;
-            questionTextLabel.Location = new Point(17, 57);
+            questionTextLabel.BackColor = Color.SteelBlue;
+            questionTextLabel.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            questionTextLabel.ForeColor = SystemColors.ControlLightLight;
+            questionTextLabel.Location = new Point(12, 425);
             questionTextLabel.Name = "questionTextLabel";
-            questionTextLabel.Size = new Size(200, 15);
+            questionTextLabel.Size = new Size(813, 70);
             questionTextLabel.TabIndex = 0;
             questionTextLabel.Text = "Здесь будет текст каждого вопроса";
+            questionTextLabel.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // answerTextButton1
             // 
-            answerTextButton1.Location = new Point(58, 113);
+            answerTextButton1.Location = new Point(129, 552);
             answerTextButton1.Name = "answerTextButton1";
             answerTextButton1.Size = new Size(136, 23);
             answerTextButton1.TabIndex = 1;
@@ -58,7 +65,7 @@
             // 
             // answerTextButton2
             // 
-            answerTextButton2.Location = new Point(58, 156);
+            answerTextButton2.Location = new Point(129, 595);
             answerTextButton2.Name = "answerTextButton2";
             answerTextButton2.Size = new Size(136, 23);
             answerTextButton2.TabIndex = 2;
@@ -68,7 +75,7 @@
             // 
             // answerTextButton3
             // 
-            answerTextButton3.Location = new Point(223, 113);
+            answerTextButton3.Location = new Point(294, 552);
             answerTextButton3.Name = "answerTextButton3";
             answerTextButton3.Size = new Size(128, 23);
             answerTextButton3.TabIndex = 3;
@@ -78,7 +85,7 @@
             // 
             // answerTextButton4
             // 
-            answerTextButton4.Location = new Point(223, 156);
+            answerTextButton4.Location = new Point(294, 595);
             answerTextButton4.Name = "answerTextButton4";
             answerTextButton4.Size = new Size(128, 23);
             answerTextButton4.TabIndex = 4;
@@ -89,7 +96,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(7, 8);
+            label1.Location = new Point(22, 534);
             label1.Name = "label1";
             label1.Size = new Size(68, 15);
             label1.TabIndex = 5;
@@ -98,17 +105,39 @@
             // currentWinningLabel
             // 
             currentWinningLabel.AutoSize = true;
-            currentWinningLabel.Location = new Point(81, 9);
+            currentWinningLabel.Location = new Point(96, 535);
             currentWinningLabel.Name = "currentWinningLabel";
             currentWinningLabel.Size = new Size(13, 15);
             currentWinningLabel.TabIndex = 6;
             currentWinningLabel.Text = "0";
             // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.Обычный_основной_фон;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(835, 423);
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.MidnightBlue;
+            pictureBox2.BackgroundImageLayout = ImageLayout.None;
+            pictureBox2.Location = new Point(0, 416);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(835, 90);
+            pictureBox2.TabIndex = 8;
+            pictureBox2.TabStop = false;
+            // 
             // GameForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(399, 191);
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(834, 661);
+            Controls.Add(pictureBox1);
             Controls.Add(currentWinningLabel);
             Controls.Add(label1);
             Controls.Add(answerTextButton4);
@@ -116,9 +145,14 @@
             Controls.Add(answerTextButton2);
             Controls.Add(answerTextButton1);
             Controls.Add(questionTextLabel);
+            Controls.Add(pictureBox2);
+            MaximizeBox = false;
+            MinimizeBox = false;
             Name = "GameForm";
             Text = "Кто хочет стать миллионером";
             Load += GameForm_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -132,5 +166,7 @@
         private Button answerTextButton4;
         private Label label1;
         private Label currentWinningLabel;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
     }
 }
